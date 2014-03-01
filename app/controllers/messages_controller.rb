6573f05d49c2b1c22f63d02f272271d9985fc7d5
Kerri Miller <kerrizor@kerrizor.com>
2014-02-28 17:05:15 -0800

    some bad code

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,18 @@ class MessagesController < ApplicationController
   def index
   end
 
-  def post_message
+  def bad_post_message
+    campfire = Tinder::Campfire.new(Figaro.env.campfire_subdomain,
+                                    token: Figaro.env.campfire_token)
+
+    room = campfire.find_room_by_name(Figaro.env.campfire_default_room)
+    room.speak(params[:message])
+
+    redirect_to :root
+  end
+
+
+  def better_post_message
     MessagePoster.say(params[:message])
 
     redirect_to :root
--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,17 +1,37 @@
 require "spec_helper"
 
 describe MessagesController do
+  subject(:the_controller) { described_class.new(required_arg: 'foo') }
   describe "GET #index" do
     it "responds successfully with an HTTP 200 status code" do
       get :index
+
       expect(response).to be_success
       expect(response.status).to eq(200)
     end
   end
 
   describe "POST #post_message" do
-    it "responds successfully with an HTTP 302 status code" do
+    xit "responds successfully with an HTTP 302 status code" do
       expect(MessagePoster).to receive(:say).and_return(true)
+      post :post_message, message: "this is a test"
+
+      expect(response).to be_redirect
+      expect(response.status).to eq(302)
+    end
+
+    xit "responds successfully with an HTTP 302 status code" do
+      expect(MessagePoster).to receive(:say).and_return(true)
+
+      post :post_message, message: "this is a test"
+
+      expect(response).to be_redirect
+      expect(response.status).to eq(302)
+    end
+
+    it "responds successfully with an HTTP 302 status code (testing against CampfirePoster)" do
+      expect(MessagePoster).to receive(:say).and_return(true)
+
       post :post_message, message: "this is a test"
 
       expect(response).to be_redirect
@@ -19,3 +39,16 @@ describe MessagesController do
     end
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+#
